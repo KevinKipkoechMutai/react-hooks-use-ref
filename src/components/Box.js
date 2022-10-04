9c04421f60a8b180cb90d238a848a67edc37adc2
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-04 13:30:42 +0300

    Completed assignment

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,10 +3,15 @@ import React, { useRef } from "react";
 function Box() {
   const elementRef = useRef();
 
+  //handlick clicking of the button
+  function handleClick() {
+    console.log("Measurements: ", elementRef.current.getBoundingClientRect());
+  }
+
   return (
     <div ref={elementRef}>
       <h1>Box</h1>
-      <button>Measure</button>
+      <button onClick={handleClick}>Measure</button>
     </div>
   );
 }
--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,10 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
 
+  //setting previous price reference
+  const prevPriceRef = useRef(price)
+
+  //setting effects to re-render only when price changes
+  useEffect(() => {
+    const previousPrice = prevPriceRef.current;
+    if (price > previousPrice) {
+      setColor('green');
+    } else if (price < previousPrice) {
+      setColor("red");
+    } else {
+      setColor("black");
+    }
+
+    prevPriceRef.current = price;
+  }, [price])
+
+  //setting effects to re-render after every change
+  
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
     return function () {
